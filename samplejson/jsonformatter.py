76e81e9b98a94541725880c9434f8148e959a2bb
Jing Hong Teh <jing.teh23@imperial.ac.uk>
2023-12-04 15:33:08 +0000

    --

--- samplejson/jsonformatter.py
+++ samplejson/jsonformatter.py
@@ -4,9 +4,9 @@
 import json
 
 
-with open("sample.json", "r") as read_file:
+with open("recipe.json", "r") as read_file:
     data = json.load(read_file)
 
 
-with open("sample.json", "w") as write_file:
+with open("recipe.json", "w") as write_file:
     json.dump(data, write_file, indent=4)
