0b25aeac650c581cfe0b8c98506b8be8732d54cf
Jing Hong Teh <jing.teh23@imperial.ac.uk>
2023-12-04 15:32:33 +0000

    Working on json

--- api/app.py
+++ api/app.py
@@ -1,8 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import json
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return(render_template("index.html"))
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    args = {}
+    args["diet"] = request.form.getlist("diet")
+    args["health"] = request.form.getlist("health")
+    args["cuisine"] = request.form.getlist("type")
+    args["dish"] = request.form.getlist("dish")
+
+    print(args)
+
+    return redirect(url_for("results"))
+
+@app.route("/results")
+def results():
+    # For Testing (Will be replaced with Requests)
+    with open("../samplejson/recipe.json", "r") as read_file:
+        data = json.load(read_file)
+    
+    result_args = {"image": []}
+    for recipe in data["hits"]:
+        result_args["image"].append(recipe["recipe"]["label"])
+    
+    print(result_args)
+
+    return render_template("results.html")
