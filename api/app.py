bca452d591fc59af63610c3f26bd4f23c2bb8331
Jing Hong Teh <jhteh2000@gmail.com>
2023-12-10 02:11:46 +0000

    Test Production

--- api/app.py
+++ api/app.py
@@ -50,12 +50,12 @@ def results():
     args_dict = json.loads(args_dict)
 
     # For Testing (Will be replaced with Requests)
-    with open(join("../samplejson", "recipe.json"), "r") as read_file:
-        data = json.load(read_file)
+    # with open(join("../samplejson", "recipe.json"), "r") as read_file:
+    #     data = json.load(read_file)
 
     # For Production
-    # response = get_response_recipe(args_dict)
-    # data = response.json()
+    response = get_response_recipe(args_dict)
+    data = response.json()
 
     result_args = {
         "count": 0,
@@ -145,12 +145,12 @@ def user_info():
         uri_list.append(dish["dish_uri"])
 
     # For Testing (Will be replaced with Requests)
-    with open(join("../samplejson", "recipe.json"), "r") as read_file:
-        data = json.load(read_file)
+    # with open(join("../samplejson", "recipe.json"), "r") as read_file:
+    #     data = json.load(read_file)
 
     # For Production
-    # response = get_response_uri(uri_list)
-    # data = response.json()
+    response = get_response_uri(uri_list)
+    data = response.json()
 
     favorites = {
         "count": 0,
