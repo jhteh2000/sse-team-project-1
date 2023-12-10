3917cc3e32820c66b7656a5aaaac493755d8152d
Robbie Yeung <ryy23@ic.ac.uk>
2023-12-10 17:08:18 +0000

    removed unnecessary

--- api/static/js/welcome.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const container = document.getElementById('welcome-container');
-
-    // Add animation class after a short delay
-    setTimeout(function() {
-        container.classList.add('show');
-    }, 500); // Adjust the delay based on your preference
-});
